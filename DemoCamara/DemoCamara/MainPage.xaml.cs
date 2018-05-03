6c781d0c7c5daa0f75e475a2ee21f07ae8c8dfb2
Ivan Toledo <itoledo@birdie.cl>
2018-05-03 18:41:23 -0300

    Add project files.

---
+++ DemoCamara/DemoCamara/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoCamara
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public async void SacarFoto(object sender, EventArgs pars)
+        {
+            CrossMedia.Current.Initialize();
+            var opts = new StoreCameraMediaOptions()
+            {
+                 
+            };
+            var foto = await CrossMedia.Current.TakePhotoAsync(opts);
+            Imagen.Source = ImageSource.FromStream(() =>
+            {
+                var stream = foto.GetStream();
+                return stream;
+            });
+        }
+    }
+}
